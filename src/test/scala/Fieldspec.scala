f99a78f06b5a5916e780b9ab2bc30b5b920c2324
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-10-27 09:38:10 +0200

    amongus

---
+++ src/test/scala/Fieldspec.scala
@@ -0,0 +1,34 @@
+package blokus.models
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+class FieldSpec extends AnyWordSpec with Matchers {
+
+  "Field" should {
+
+    val testField = new Field(5, 5)
+
+    "convert a row of integers to characters correctly" in {
+      val row = Array(0, 1, 2, 1, 0)
+      testField.rowToString(row) shouldBe "# + ? + # "
+    }
+
+    "convert the array to a string correctly" in {
+      val initialString = "+ + + + + \n" * 5
+      testField.createFieldString shouldBe initialString.trim
+    }
+
+    "change the field correctly" in {
+      testField.changeField(2, 2, 0)
+      val changedArray = testField.getFieldArray
+      changedArray(2)(2) shouldBe 0
+    }
+
+    "throw an assertion error for invalid x and y coordinates" in {
+      assertThrows[AssertionError] {
+        testField.changeField(6, 6, 0)
+      }
+    }
+  }
+}
