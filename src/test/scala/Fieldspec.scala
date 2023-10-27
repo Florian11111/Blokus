e023c43f12f3a7b2375f96dc9577b0f61b6bf8c9
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-10-27 11:47:06 +0200

    commit AMOGZS

--- src/test/scala/Fieldspec.scala
+++ src/test/scala/Fieldspec.scala
@@ -7,29 +7,46 @@ class FieldSpec extends AnyWordSpec with Matchers {
 
   "Field" should {
 
-    val testField = Field(5, 5)
-
-    "convert a row of integers to characters correctly" in {
-      val row = Vector(0, 1, 2, 1, 0)
-      testField.rowToString(row) shouldBe "# + ? + # "
+    "be initialized with the correct dimensions" in {
+      val testField = Field(5, 5)
+      testField.width shouldBe 5
+      testField.height shouldBe 5
     }
 
-    "convert the vector to a string correctly" in {
-      val initialString = ("+ + + + + " + "\n") * 4 + "+ + + + + "
-      testField.createFieldString shouldBe initialString
+    "initialize all values to 1" in {
+      val testField = Field(5, 5)
+      for {
+        row <- 0 until 5
+        col <- 0 until 5
+      } testField.getFieldVector(row)(col) shouldBe 1
+    }
+
+    "convert a row of integers to characters correctly" in {
+      val testField = Field(5, 5)
+      testField.rowToString(Vector(1, 0, 1, 2, 1)) shouldBe "+ # + ? + "
+    }
+
+    "convert the field vector to a string correctly" in {
+      val testField = Field(2, 2)
+      testField.createFieldString shouldBe "+ + \n+ + "
     }
 
     "change the field correctly" in {
-      val updatedField = testField.changeField(2, 2, 0) // Create a new Field with the updated state
-      val changedVector = updatedField.getFieldVector
-      changedVector(2)(2) shouldBe 0
+      val testField = Field(5, 5)
+      val changedField = testField.changeField(2, 2, 0)
+      changedField.getFieldVector(2)(2) shouldBe 0
     }
 
-
-    "throw an assertion error for invalid x and y coordinates" in {
+    "handle an invalid changeField request by throwing an assertion error" in {
+      val testField = Field(5, 5)
       assertThrows[AssertionError] {
         testField.changeField(6, 6, 0)
       }
+    }
+
+    "return the correct width even if the field vector is empty" in {
+      val emptyField = new Field(Vector.empty)
+      emptyField.width shouldBe 0
     }
   }
 }
