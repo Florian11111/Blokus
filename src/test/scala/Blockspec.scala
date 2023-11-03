4df7fc1bd27fdf77eff55ef904cf25d33ff801c7
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-11-03 09:10:21 +0100

    Update Blockspec.scala

--- src/test/scala/Blockspec.scala
+++ src/test/scala/Blockspec.scala
@@ -1,41 +1,37 @@
 package blokus.models
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class BlockTypeSpec extends AnyFlatSpec with Matchers {
+class BlockTypeTest extends AnyFlatSpec with Matchers {
 
-  "BlockType" should "correctly rotate the block 90 degrees" in {
-    val result = BlockType.createBlock(BlockType.blockA, 1, false)
-    result should contain theSameElementsAs List((0, 0), (-1, 0), (1, 0), (-1, 1))
+  "createBlock" should "return original block if no rotation or mirroring is done" in {
+    val block = BlockType.createBlock(BlockType.block1, 0, false)
+    block should contain theSameElementsAs BlockType.block1.baseForm
   }
 
-  it should "correctly rotate the block 180 degrees" in {
-    val result = BlockType.createBlock(BlockType.blockA, 2, false)
-    result should contain theSameElementsAs List((0, 0), (0, 1), (0, -1), (-1, -1))
+  it should "mirror the block correctly" in {
+    val mirroredBlock = BlockType.createBlock(BlockType.block2, 0, true)
+    mirroredBlock should contain theSameElementsAs List((0, 0), (1, 0))
   }
 
-  it should "correctly rotate the block 270 degrees" in {
-    val result = BlockType.createBlock(BlockType.blockA, 3, false)
-    result should contain theSameElementsAs List((0, 0), (1, 0), (-1, 0), (-1, -1))
+  it should "rotate the block 90 degrees correctly" in {
+    val rotatedBlock = BlockType.createBlock(BlockType.block4, 1, false)
+    rotatedBlock should contain theSameElementsAs List((0, 0), (0, -1), (1, 0))
   }
 
-  it should "correctly rotate the block 360 degrees (which should be same as original)" in {
-    val result = BlockType.createBlock(BlockType.blockA, 4, false)
-    result should contain theSameElementsAs BlockType.blockA.baseForm
+  it should "rotate the block 180 degrees correctly" in {
+    val rotatedBlock = BlockType.createBlock(BlockType.block4, 2, false)
+    rotatedBlock should contain theSameElementsAs List((0, 0), (-1, 0), (0, -1))
   }
 
-  it should "correctly mirror the block" in {
-    val result = BlockType.createBlock(BlockType.blockA, 0, true)
-    result should contain theSameElementsAs List((0, 0), (0, 1), (0, -1), (1, -1))
+  it should "rotate the block 270 degrees correctly" in {
+    val rotatedBlock = BlockType.createBlock(BlockType.block4, 3, false)
+    rotatedBlock should contain theSameElementsAs List((0, 0), (0, 1), (-1, 0))
   }
 
-  it should "rotate and then mirror the block" in {
-    val result = BlockType.createBlock(BlockType.blockA, 1, true)
-    result should contain theSameElementsAs List((0, 0), (-1, 0), (1, 0), (1, -1))
-  }
-
-  "BlockB" should "be correctly represented in its base form" in {
-    BlockType.blockB.baseForm should contain theSameElementsAs List((0, 0), (0, -1), (1, 0), (1, -1))
+  it should "rotate 90 degrees and mirror the block correctly" in {
+    val rotatedAndMirroredBlock = BlockType.createBlock(BlockType.block4, 1, true)
+    rotatedAndMirroredBlock should contain theSameElementsAs List((0, 0), (1, 0), (0, 1))
   }
 }
-
