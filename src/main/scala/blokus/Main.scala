482a5d57fd8558a033a59440ac588c1aed59c250
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-11-03 10:14:44 +0100

    Update Main.scala

--- src/main/scala/blokus/Main.scala
+++ src/main/scala/blokus/Main.scala
@@ -3,9 +3,35 @@ package blokus
 import models.Field
 import models.Block
 
-import scala.io.StdIn
-
 object Main {
+
+  def configureTerminalForImmediateInput(): Unit = {
+    val cmd = "stty -icanon min 1 -echo"
+    val pb = new ProcessBuilder("sh", "-c", cmd)
+    pb.inheritIO().start().waitFor()
+  }
+
+  def clearScreen(): Unit = {
+    val os = System.getProperty("os.name").toLowerCase
+    os match {
+      case name if name.contains("win") =>
+        Runtime.getRuntime.exec("cls")
+      case _ =>
+        Runtime.getRuntime.exec("clear")
+    }
+  }
+
+
+  def captureKeyPress(): Char = {
+    System.in.read().toChar
+  }
+
+  def resetTerminalToNormal(): Unit = {
+    val cmd = "stty echo icanon"
+    val pb = new ProcessBuilder("sh", "-c", cmd)
+    pb.inheritIO().start().waitFor()
+  }
+
   def main(args: Array[String]): Unit = {
     val width = 20 // Hier die Breite deines Felds eingeben
     val height = 20 // Hier die Höhe deines Felds eingeben
@@ -17,55 +43,53 @@ object Main {
     var rotation = 0
     var mirrored = false
 
-    while (true) {
-      val currentBlock = Block.createBlock(currentBlockType, rotation, mirrored)
-      println(renderBlock(currentBlock, blockField, currentX, currentY).createFieldString)
+    configureTerminalForImmediateInput()
 
-      println("Steuerung:")
-      println("Pfeiltasten: Block bewegen")
-      println("r: Block platzieren")
-      println("q: Beenden")
-      
-      def renderBlock(block: List[(Int, Int)], fieldOld: Field, x: Int, y: Int): Field = {
-        var newField = Field(width, height)
-        for ((dx, dy) <- block) {
-          val newX = x + dx
-          val newY = y + dy
-          if (newX >= 0 && newX < width && newY >= 0 && newY < height) {
-            newField = newField.changeField(newX, newY, 0)
+    try {
+      while (true) {
+        clearScreen() // Terminal löschen
+        val currentBlock = Block.createBlock(currentBlockType, rotation, mirrored)
+        println(renderBlock(currentBlock, blockField, currentX, currentY, width, height).createFieldString)
+
+        println("Steuerung:")
+        println("Pfeiltasten: Block bewegen")
+        println("r: Block platzieren")
+        println("q: Beenden")
+
+        val input = captureKeyPress()
+
+        input match {
+          case 'q' => System.exit(0)
+          case 'r' => {
+            blockField = blockField.placeBlock(currentBlock, currentX, currentY, 2)
+            println(blockField.createFieldString)
+            currentBlockType = Block.block15
+            currentX = 0
+            currentY = 0
           }
-        }
-        newField
-      }
-
-
-      val input = StdIn.readLine()
-
-      input match {
-        case "q" => System.exit(0)
-        case "r" => {
-          blockField = blockField.placeBlock(currentBlock, currentX, currentY, 2)
-          println(blockField.createFieldString)
-          currentBlockType = Block.block15
-          currentX = 0
-          currentY = 0
-        }
-        case _ => {
-          val dx = input match {
-            case "w" => -1
-            case "s" => 1
-            case _ => 0
-          }
-          val dy = input match {
-            case "a" => -1
-            case "d" => 1
-            case _ => 0
-          }
-
-          currentX += dx
-          currentY += dy
+          case 'w' => currentX -= 1
+          case 's' => currentX += 1
+          case 'a' => currentY -= 1
+          case 'd' => currentY += 1
+          case _ => // ignoriere andere Tastendrücke
         }
       }
+    } finally {
+      resetTerminalToNormal()
     }
   }
+
+    def renderBlock(block: List[(Int, Int)], fieldOld: Field, x: Int, y: Int, width: Int, height: Int): Field = {
+    var newField = Field(width, height)
+    for ((dx, dy) <- block) {
+      val newX = x + dx
+      val newY = y + dy
+      if (newX >= 0 && newX < width && newY >= 0 && newY < height) {
+        newField = newField.changeField(newX, newY, 0)
+      }
+    }
+    newField
+  }
+
+
 }
