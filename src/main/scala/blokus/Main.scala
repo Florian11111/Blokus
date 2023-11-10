6dc043e54c0898fe27ecf96cecfa796fe05a589a
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-11-10 09:08:09 +0100

    commite die amongus

--- src/main/scala/blokus/Main.scala
+++ src/main/scala/blokus/Main.scala
@@ -1,36 +1,13 @@
 package blokus
 
-import models.Controller
+import blokus.controller.Controller
+import blokus.view.Tui
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val controller = new Controller()
-    
-    while (true) {
-      println(controller.renderField())
-
-      println("Steuerung:")
-      println("Pfeiltasten: Block bewegen")
-      println("r: Block platzieren")
-      println("q: Beenden")
-      def captureKeyPress(): Char = {
-          System.in.read().toChar
-      }
-
-      val input = captureKeyPress()
-
-      input match {
-        case 'q' => System.exit(0)
-        case 'r' => {
-          controller.setzen()
-        }
-        case 'w' => controller.move(3) // Bewegung nach oben
-        case 's' => controller.move(1) // Bewegung nach unten
-        case 'a' => controller.move(2) // Bewegung nach links
-        case 'd' => controller.move(0) // Bewegung nach rechts
-        case _ => // ignoriere andere Tastendrücke
-      }
-    }
+    val controller = new Controller(1, 20, 20)
+    val tui = new Tui(controller)
+    tui.inputLoop()
   }
 }
--- src/main/scala/blokus/view/tui.scala
+++ src/main/scala/blokus/view/tui.scala
@@ -1,5 +1,52 @@
 package blokus.view
 
-class tui {
-  
+import blokus.models.Field
+import blokus.controller.Controller
+import scala.io.StdIn
+
+class Tui(controller: Controller) {
+  def configureTerminalForImmediateInput(): Unit = {
+    // Implementation remains the same
+  }
+
+  def clearTerminal(): Unit = {
+    print("\u001b[H\u001b[2J")
+    System.out.flush()
+  }
+
+  def captureKeyPress(): Char = {
+    StdIn.readChar()
+  }
+
+  def resetTerminalToNormal(): Unit = {
+    // Implementation remains the same
+  }
+
+  def inputLoop(): Unit = {
+    configureTerminalForImmediateInput()
+    try {
+      var continue = true
+      while (continue) {
+        clearTerminal()
+        controller.display() // bullshit
+
+        println("Steuerung:")
+        println("w/a/s/d: Block bewegen")
+        println("q: Block rotieren")
+        println("e: Block spiegeln")
+        println("Enter: Block platzieren")
+        println("x: Beenden")
+
+        val input = captureKeyPress()
+        input match {
+          case 'x' =>
+            continue = false
+          case _ =>
+            controller.handleInput(input) //bullshit, muss hier sein.
+        }
+      }
+    } finally {
+      resetTerminalToNormal()
+    }
+  }
 }
