810bc3c7b38b6170ad91481ed784dcbbca765b78
Florian11111 <116258400+Florian11111@users.noreply.github.com>
2023-11-10 08:48:42 +0100

    height and width update

--- src/main/scala/blokus/controller/Controller.scala
+++ src/main/scala/blokus/controller/Controller.scala
@@ -4,16 +4,13 @@ import blokus.models.Field
 import blokus.models.Block
 import blokus.models.HoverBlock
 
-class Controller(playerAmount: Int) {
-    val width = 20 // Hier die Breite deines Felds eingeben
-    val height = 20 // Hier die Höhe deines Felds eingeben
+class Controller(playerAmount: Int, width: Int, height: Int) {
     var field = Field(width, height)
     var hoverBlock = HoverBlock(playerAmount)
     
-    def changePlayer = hoverBlock.changePlayer()
     def move(field:Field, richtung:Int) = hoverBlock.move(field, richtung)
     def rotate(field:Field) = hoverBlock.rotate(field:Field)
     def mirror(field:Field) = hoverBlock.mirror(field:Field)
     def setzen(neuerTyp: Int) = hoverBlock.setzen(field, neuerTyp)
-
+    def changePlayer = hoverBlock.changePlayer()
 }
