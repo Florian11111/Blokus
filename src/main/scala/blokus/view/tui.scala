67082323da98756235596b363e675c5cc5027301
Florian11111 <116258400+Florian11111@users.noreply.github.com>
2023-11-10 12:14:11 +0100

    s

--- src/main/scala/blokus/view/tui.scala
+++ src/main/scala/blokus/view/tui.scala
@@ -1,9 +1,11 @@
 package blokus.view
 
 import blokus.controller.Controller
+import blokus.controller.ControllerEvent
 import blokus.util.Observer
 
 class Tui(controller: Controller) extends Observer[ControllerEvent] {
+
     controller.addObserver(this)
 
     def clearTerminal(): Unit = {
@@ -15,12 +17,12 @@ class Tui(controller: Controller) extends Observer[ControllerEvent] {
         val field = controller.getField()
         val block = controller.getBlock()
         val merged = field.zipWithIndex.map { case (row, rowIndex) =>
-            row.zipWithIndex.map { case (cell, columnIndex) =>
-                val blockCell = block.find { case (x, y) =>
-                    x == columnIndex && y == rowIndex
-                }
-                blockCell.map(_ => playerNumber).getOrElse(cell)
+        row.zipWithIndex.map { case (cell, columnIndex) =>
+            val blockCell = block.find { case (x, y) =>
+            x == columnIndex && y == rowIndex
             }
+            blockCell.map(_ => playerNumber).getOrElse(cell)
+        }
         }
         merged
     }
@@ -31,11 +33,11 @@ class Tui(controller: Controller) extends Observer[ControllerEvent] {
 
     def rowToString(row: Vector[Int]): String = {
         row.map {
-            case 0 => "# "
-            case 1 => "+ "
-            case 2 => "| "
-            case 6 => "X "
-            case _ => "? "
+        case 0 => "# "
+        case 1 => "+ "
+        case 2 => "| "
+        case 6 => "X "
+        case _ => "? "
         }.mkString
     }
 
@@ -48,27 +50,40 @@ class Tui(controller: Controller) extends Observer[ControllerEvent] {
         println("m: Block spiegeln")
         println("s: Block platzieren")
         println("x: Beenden")
+
+        event match {
+        case ControllerEvent.Move =>
+            // Handle the move event if needed
+        case ControllerEvent.Rotate =>
+            // Handle the rotate event if needed
+        case ControllerEvent.Mirror =>
+            // Handle the mirror event if needed
+        case ControllerEvent.Setzen =>
+            // Handle the setzen event if needed
+        case ControllerEvent.PlayerChange(player) =>
+            // Handle the player change event if needed
+        }
     }
 
     def inputLoop(): Unit = {
         try {
-            var continue = true
-            while (continue) {
-                val input = scala.io.StdIn.readLine()
-                input match {
-                    case "x" => continue = false
-                    case "w" => controller.move(2)
-                    case "d" => controller.move(1)
-                    case "s" => controller.move(0)
-                    case "a" => controller.move(3)
-                    case "r" => controller.rotate()
-                    case "m" => controller.mirror()
-                    case "e" => controller.setzen(2)
-                    case _ =>
-                }
+        var continue = true
+        while (continue) {
+            val input = scala.io.StdIn.readLine()
+            input match {
+            case "x" => continue = false
+            case "w" => controller.move(2)
+            case "d" => controller.move(1)
+            case "s" => controller.move(0)
+            case "a" => controller.move(3)
+            case "r" => controller.rotate()
+            case "m" => controller.mirror()
+            case "e" => controller.setzen(2)
+            case _ =>
             }
+        }
         } finally {
-            // Reset any terminal configurations if needed
+        // Reset any terminal configurations if needed
         }
     }
 }
