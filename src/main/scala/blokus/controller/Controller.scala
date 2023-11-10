159b33f3415f2511026a144652af504b6e191267
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-11-10 12:10:38 +0100

    ainniaaniia

--- src/main/scala/blokus/controller/Controller.scala
+++ src/main/scala/blokus/controller/Controller.scala
@@ -3,19 +3,59 @@ package blokus.controller
 import blokus.models.Field
 import blokus.models.Block
 import blokus.models.HoverBlock
+import blokus.util.{Observable, Observer}
 
-class Controller(playerAmount: Int, firstBlock: Int, width: Int, height: Int) {
+class Controller(playerAmount: Int, firstBlock: Int, width: Int, height: Int) extends Observable[ControllerEvent] {
 
     var field = Field(width, height)
     var hoverBlock = HoverBlock(playerAmount, firstBlock)
-    
-    def getfield(): Vector[Vector[Int]] = field.getFieldVector
+
+    def getField(): Vector[Vector[Int]] = field.getFieldVector
     def getBlock(): List[(Int, Int)] = hoverBlock.getBlock()
-    def move(richtung:Int) = hoverBlock.move(field, richtung)
-    def rotate():Boolean = hoverBlock.rotate(field)
-    def mirror():Boolean = hoverBlock.mirror(field)
-    def setzen(neuerTyp: Int) = field = hoverBlock.setzen(field, neuerTyp)
-    def getRotation() = hoverBlock.getRotation()
-    def changePlayer = hoverBlock.changePlayer()
-    def getCurrentPlayer = hoverBlock.getCurrentPlayer
+    def move(richtung: Int): Boolean = {
+        val moved = hoverBlock.move(field, richtung)
+        if (moved) {
+            notifyObservers(ControllerEvent.Move)
+        }
+        moved
+    }
+
+    def rotate(): Boolean = {
+        val rotated = hoverBlock.rotate(field)
+        if (rotated) {
+            notifyObservers(ControllerEvent.Rotate)
+        }
+        rotated
+    }
+
+    def mirror(): Boolean = {
+        val mirrored = hoverBlock.mirror(field)
+        if (mirrored) {
+            notifyObservers(ControllerEvent.Mirror)
+        }
+        mirrored
+    }
+
+    def setzen(neuerTyp: Int): Unit = {
+        field = hoverBlock.setzen(field, neuerTyp)
+        notifyObservers(ControllerEvent.Setzen)
+    }
+
+    def getRotation(): Int = hoverBlock.getRotation()
+    def changePlayer(): Int = {
+        val currentPlayer = hoverBlock.changePlayer()
+        notifyObservers(ControllerEvent.PlayerChange(currentPlayer))
+        currentPlayer
+    }
+
+    def getCurrentPlayer: Int = hoverBlock.getCurrentPlayer
+}
+
+sealed trait ControllerEvent
+object ControllerEvent {
+    case object Move extends ControllerEvent
+    case object Rotate extends ControllerEvent
+    case object Mirror extends ControllerEvent
+    case object Setzen extends ControllerEvent
+    case class PlayerChange(player: Int) extends ControllerEvent
 }
