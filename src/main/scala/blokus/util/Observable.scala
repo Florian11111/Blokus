3539625efd679a3daf7112c52426cf6123b65b8f
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2023-11-10 11:59:47 +0100

    themongus

--- src/main/scala/blokus/util/Observable.scala
+++ src/main/scala/blokus/util/Observable.scala
@@ -1,5 +1,12 @@
 package blokus.util
 
-class Observable {
+trait Observer[E] {
+    def update(e: E): Unit
+}
 
+class Observable[E] {
+    private var subscribers: Vector[Observer[E]] = Vector()
+    def addObserver(o: Observer[E]): Unit = subscribers = subscribers :+ o
+    def removeObserver(o: Observer[E]): Unit = subscribers = subscribers.filterNot(_ == o)
+    def notifyObservers(e: E): Unit = subscribers.foreach(_.update(e))
 }
