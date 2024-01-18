4c582ef3a61ae52bce127a23dc116fbd39ccd9bb
Florian11111 <116258400+Florian11111@users.noreply.github.com>
2024-01-18 13:08:17 +0100

    removed Comand

--- src/main/scala/de/htwg/se/blokus/models/GameState.scala
+++ src/main/scala/de/htwg/se/blokus/models/GameState.scala
@@ -2,7 +2,7 @@ package de.htwg.se.blokus.models
 
 import de.htwg.se.blokus.models.FieldInterface
 import de.htwg.se.blokus.models.BlockInventoryInterface
-/*  */
+
 class GameState(private var field: FieldInterface,
                 private var currentPlayer: Int,
                 private var blockInventory: BlockInventoryInterface) {
